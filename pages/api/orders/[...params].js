a9c62e2f1cf3a6392f5865a65865171279e42705
Marco Aurélio <78933454+marcoaureliotw2@users.noreply.github.com>
2021-10-13 11:44:35 -0300

    Update [...params].js

--- pages/api/orders/[...params].js
+++ pages/api/orders/[...params].js
@@ -33,10 +33,10 @@ const orderDatails = {
     },
     items: [
       {
-        id: '22815773000169-732bd31e-77fc-47ee-88ee-a0437f97b198',
+        id: '22815773000169-F41D4B93-31A8-4043-A3D6-000005935FEC',
         index: 0,
-        name: 'Medium - 2 flavors',
-        externalCode: '22',
+        name: 'CESTA RECHEADA',
+        externalCode: '640414',
         unit: 'UNIT',
         quantity: 1,
         specialInstructions: 'Do not put onions.',
@@ -54,7 +54,7 @@ const orderDatails = {
         },
         options: [
           {
-            id: '22815773000169-e5232f14-430c-4a94-8ff6-289d5a16a87a',
+            id: '22815773000169-F41D4B93-31A8-4043-A3D6-000005935FEC',
             name: '1/2 Pepperoni',
             externalCode: '23',
             unit: 'UNIT',
@@ -70,7 +70,7 @@ const orderDatails = {
             specialInstructions: 'None',
           },
           {
-            id: '22815773000169-7e507cab-7235-4f75-a0c2-e955fb2f2048',
+            id: '22815773000169-F41D4B93-31A8-4043-A3D6-000005935FEC',
             name: '1/2 Mozzarella',
             externalCode: '24',
             unit: 'UNIT',
